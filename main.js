2107c9ccf801e4787777836bca805608d6f319fa
Missy Bur <missybur@gmail.com>
2015-10-30 13:41:58 -0700

    Add function to calculator

---
+++ main.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+  var testNumLength = function(number) {
+    if (number.length > 9) {
+      totaldiv.text(number.substr(number.length-9,9));
+      if (number.length > 15) {
+        number = "";
+        totaldiv.text("Err");
+      }
+    }
+  };
+  var number = "";
+  var newnumber = "";
+  var operator = "";
+  var totaldiv = $("#total");
+  totaldiv.text("0");
+  $("#numbers > a").not("#clear,#clearall").click(function(){
+    number += $(this).text();
+    totaldiv.text(number);
+    testNumLength(number);
+  });
+  $("#operators > a").not("#equals").click(function(){
+    operator = $(this).text();
+    newnumber = number;
+    number = "";
+    totaldiv.text("0");
+  });
+
+  $("#plusMinus").click(function(){
+    number += $(this).text();
+    // number = newnumber * -1;
+  })
+
+  $("#clear,#clearall").click(function(){
+    number = "";
+    totaldiv.text("0");
+    if ($(this).attr("id") === "clearall") {
+      newnumber = "";
+    }
+  });
+  $("#equals").click(function(){
+    number = parseInt(number, 10);
+    newnumber = parseInt(newnumber, 10);
+    var result;
+    if(operator === "+") {
+     result = newnumber + number;
+   }
+   else if(operator === "%") {
+    result = newnumber / 100;
+  }
+
+  else if(operator === "-") {
+   result = newnumber - number;
+ }
+ else if(operator === "x") {
+   result = newnumber * number;
+ }
+ else if(operator === "/") {
+   result = newnumber / number;
+ }
+
+ result = result.toString(10);
+ totaldiv.text(result);
+ testNumLength(result);
+ number = "";
+ newnumber = "";
+});
+  });
